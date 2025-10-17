72b29315df6232f14463c70b3a3741ebf5f9f132
VladFomin17 <fominvladosik@gmail.com>
2025-10-18 00:37:39 +0300

    add product

--- src/Product.java
+++ src/Product.java
@@ -1,4 +1,38 @@
-package PACKAGE_NAME;
-
+/**
+ * Товар
+ */
 public class Product {
+    private String _name;
+    private double _price;
+
+    /**
+     * Конструктор класса
+     *
+     * @param name
+     * @param price
+     */
+    public Product(String name, double price) {
+        this._name = name;
+        this._price = price;
+    }
+
+    /**
+     * Возвращает имя товара
+     *
+     * @return имя товара
+     */
+    public String getName() {
+        return _name;
+    }
+
+    /**
+     * Переопределяет метод toString для объектов класса
+     *
+     * @return строка с информацией о товаре
+     */
+    @Override
+    public String toString() {
+        return String.format("%s (%.2f руб.)", _name, _price);
+    }
+
 }
